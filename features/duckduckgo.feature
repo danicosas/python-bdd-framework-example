b3c79c0bd02dd3b7f00ee14f12582ee179eda33c
Dani <danicosas+github@pm.me>
2023-08-26 09:19:00 +0100

    Chapter 10

--- features/duckduckgo.feature
+++ features/duckduckgo.feature
@@ -1,6 +1,12 @@
 Feature: Searching on DuckDuckGo
 
-  Scenario: Searching for a term
+  Scenario Outline: Searching for terms from file
     Given I am on the DuckDuckGo homepage
-    When I search for the term "Duck"
-    Then I see search results related to the search term
+    When I search for the term "<SearchTerm>"
+    Then I see search results related to the term "<SearchTerm>"
+
+    Examples:
+      | SearchTerm |
+      | Duck       |
+      | Python     |
+      | Behave     |
--- steps/duckduckgo_steps.py
+++ steps/duckduckgo_steps.py
@@ -1,19 +1,31 @@
+import json
+import os
 from behave import given, when, then
 from pages.duckduckgo_page import DuckDuckGoPage
+
+# Ruta para el archivo JSON en la carpeta "data"
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'search_data.json')
+
+with open(file_path) as json_file:
+    search_data = json.load(json_file)
 
 @given('I am on the DuckDuckGo homepage')
 def step_impl(context):
     duckduckgo_page = context.page_manager.get_page(DuckDuckGoPage)
     duckduckgo_page.load(context.base_url)
 
-@when('I search for the term "{search_term}"')
-def step_impl(context, search_term):
-    duckduckgo_page = context.page_manager.get_page(DuckDuckGoPage)
-    duckduckgo_page.enter_search(search_term)
-    duckduckgo_page.click_search_button()
+@when('I search for the term "{SearchTerm}"')
+def step_impl(context, SearchTerm):
+    for search in search_data['searches']:
+        if search['term'] == SearchTerm:
+            duckduckgo_page = context.page_manager.get_page(DuckDuckGoPage)
+            duckduckgo_page.enter_search(search['term'])
+            duckduckgo_page.click_search_button()
 
-    context.search_term = search_term
+            context.search_term = search['term']
+            context.expected_result = search['expected_result']
 
-@then('I see search results related to the search term')
-def step_impl(context):
+@then('I see search results related to the term "{SearchTerm}"')
+def step_impl(context, SearchTerm):
     assert context.search_term in context.driver.title
+    assert context.expected_result in context.driver.title
